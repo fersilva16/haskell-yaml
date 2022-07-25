901c10284a90a90c8367fd846d3331ec098f7ad6
Fernando Silva <fernandonsilva16@gmail.com>
2022-07-25 11:01:42 -0300

    feat(parser): add charP and stringP (#2)

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,49 @@
 module Main where
 
+import Data.Map (Map)
+import qualified Data.Map as Map
+import System.Environment (getArgs)
+
+data YamlValue
+  = YamlNull
+  | YamlBool Bool
+  | YamlNumber Integer
+  | YamlString String
+  | YamlList [YamlValue]
+  | YamlScalar [(String, YamlValue)]
+  deriving (Show)
+
+newtype Parser a = Parser
+  { runParser :: String -> Maybe (String, a)
+  }
+
+instance Functor Parser where
+  fmap f (Parser p1) =
+    Parser $ \y -> do
+      (y', x) <- p1 y
+      return (y', f x)
+
+instance Applicative Parser where
+  pure x = Parser (\y -> Just (y, x))
+  (Parser p1) <*> (Parser p2) =
+    Parser $ \y -> do
+      (y', f) <- p1 y
+      (y'', a) <- p2 y'
+      return (y'', f a)
+
+charP :: Char -> Parser Char
+charP x = Parser f
+  where
+    f y
+      | head y == x = Just (tail y, x)
+      | otherwise = Nothing
+
+stringP :: String -> Parser String
+stringP = traverse charP
+
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = do
+  args <- getArgs
+  content <- readFile $ unwords args
+
+  print content
